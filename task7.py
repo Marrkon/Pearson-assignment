ef1231290a4e2e7dc1ea051c311ad0d66d297736
Marcin Konieczny <marcin.konieczny.poznan@gmail.com>
2020-08-17 15:54:02 +0200

    refactor: MODIFY testing func on pd.util.testing.assert_frame_equal()

--- task7.py
+++ task7.py
@@ -5,15 +5,13 @@ import unittest
 
 class DFTests(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self, filename):
         """ Check set up of the csv """
         
         path = 'datasets/'
-        test_file_name =  'test.csv'
         
         try:
-            data = pd.read_csv(path + test_file_name,
-                sep = ';')
+            data = pd.read_csv(path + filename, sep = ';')
             
         except IOError:
             print ('Cannot open the file')
@@ -21,25 +19,26 @@ class DFTests(unittest.TestCase):
         self.fixture = data
 
     def check_NaN_filtering(self):
-        """ Check if function in task2 clear the dataset in a proper way """
-
+        """ Check if function in task2 clear the dataset in a proper way """        
+        empty_df = pd.DataFrame([],columns = ['id', 'student_id', 'class_id',  'created_at',  'updated_at', 'last_event_time',  'overall_score', 'test_status',	'institution_id', 'authorized_at', 'confidence_level',  'speaking_score', 'writing_score',  'reading_score',  'listening_score',  'test_level_id',  'licence_id'])
 
         # Test few cases of data filtering from task2  
-        print("Here", self.fixture.iloc[1])
-
-        case1 = check_files_correctness(self.fixture.iloc[1])
-        case2 = check_files_correctness(self.fixture.iloc[2])
-        case3 = check_files_correctness(self.fixture.iloc[3])
-        case4 = check_files_correctness(self.fixture.iloc[4])
-
-        try:
-            assert_frame_equal(case1, [])
-            assert_frame_equal(case2, [])
-            assert_frame_equal(case3, [])
-            assert_frame_equal(case4, [])
-        except:
-            print('Assert error')
+        case1 = check_files_correctness(self.fixture.iloc[2:3,])
+        case2 = check_files_correctness(self.fixture.iloc[3:4,:])
+        case3 = check_files_correctness(self.fixture.iloc[37:38,])
+        case4 = check_files_correctness(self.fixture.iloc[41:42,])
+    
+        # If left != right rise an error
+        assert_frame_equal(case3, self.fixture.iloc[37:38,])
+   
+     
         
 if __name__ == '__main__':
-    DFTests.setUp(unittest)
+    # Filename which opening will be tested
+    filename =  'test.csv'
+
+    # Test part 
+    DFTests.setUp(unittest, filename)
     DFTests.check_NaN_filtering(unittest)
+
+    print("Test finished!")
